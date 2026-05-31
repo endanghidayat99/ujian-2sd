6094e65c4d60283967cb0593ccd22de1fedee03f
endang hidayat <endang.hidayat@collega.co.id>
2026-05-31 12:28:13 +0700

    add senibudaya

--- src/data/index.js
+++ src/data/index.js
@@ -4,6 +4,7 @@ import bahasaSunda from './bahasaSunda'
 import pkn from './pkn'
 import penjaskes from './penjaskes'
 import agama from './agama'
+import seniBudaya from "./seniBudaya.js";
 
 export const subjects = [
   matematika,
@@ -12,9 +13,10 @@ export const subjects = [
   pkn,
   penjaskes,
   agama,
+    seniBudaya,
 ]
 
-export { matematika, bahasaIndonesia, bahasaSunda, pkn, penjaskes, agama }
+export { matematika, bahasaIndonesia, bahasaSunda, pkn, penjaskes, agama, seniBudaya }
 
 export function getSubjectById(id) {
   return subjects.find(s => s.id === id)
